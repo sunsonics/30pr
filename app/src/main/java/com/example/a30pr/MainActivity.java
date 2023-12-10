2e1363a8a7fbb44ef55040aa8e395803eb1ad550
Julia <yliua.com22@gmail.com>
2023-12-11 00:18:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a30pr/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.a30pr;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    final String LOG_TAG = "myLogs";
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClickStart(View v) {
+        startService(new Intent(this, MyService.class));
+    }
+
+    public void onClickStop(View v) {
+        stopService(new Intent(this, MyService.class));
+    }
+}
